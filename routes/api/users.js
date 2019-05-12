200c2f926691abfb6bf3bd8a5334a9df0b56197d
evaizli <everrich2018@gmail.com>
2019-05-12 10:25:18 -0700

    eidt users.js routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -24,7 +24,6 @@ router.get('/current', passport.authenticate('jwt', { session: false }), (req, r
 router.post('/register', (req, res) =>{
 
     const {errors, isValid} = validateRegisterInput(req.body);
-
     if(!isValid){
         return res.status(400).json(errors);
     }
