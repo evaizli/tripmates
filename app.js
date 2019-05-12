eb65dc90f09b37335a8aff01f49c1701b5822e34
Jeff Dam <jeff.dam@gmail.com>
2019-05-12 12:43:45 -0700

    Add frontend files

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!!");
+    res.send('"Hello World!!!!!!"');
 
 });
 
