82280cc9f1b12e57790de0e2a94d3bf429e6d050
evaizli <everrich2018@gmail.com>
2019-05-11 17:37:08 -0700

    add login validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
-// const keys = require('../../config/keys');
+const keys = require('../../config/keys');
+const jwt = require('jsonwebtoken');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
@@ -47,7 +47,24 @@ router.post("/login", (req, res) =>{
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if(isMatch){
-                    res.json({msg: "Success!" });
+                    // res.json({msg: "Success!" });  REMOVED AND REPLAY WITH PAYLOD
+                        const payload ={
+                            id: user.id,
+                            handle: user.handle,
+                            email: user.email
+                        };
+
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            {expiresIn: 3600},
+                            (err, token) =>{
+                                res.json({
+                                    sucess: true,
+                                    token: "Bearer" + token 
+                                });
+                            }
+                        );
                     } else {
                         return res.status(400).json({password: "incorrect password"});
                     }
