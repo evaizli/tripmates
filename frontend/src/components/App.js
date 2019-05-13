b40e541dd44a97cb3850c9335fe2057c9cfc8aea
Jeff Dam <jeff.dam@gmail.com>
2019-05-12 20:03:06 -0700

    Correct merge

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-// import { Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 // import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import Modal from './shared/modal';
 import TripsDashContainer from './trips_dash/trips_dash_container';
 import Splash from './splash/splash';
+import { Route } from 'react-router-dom';
 
 const App = () => {
 
