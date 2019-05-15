fd0d77f93bd2b6d2cbfbadcd445bba0a5f468941
evaizli <everrich2018@gmail.com>
2019-05-14 21:27:09 -0700

    finish destination reducer

--- frontend/src/reducers/destinations_reducer.js
+++ frontend/src/reducers/destinations_reducer.js
@@ -4,9 +4,33 @@ import {
   REMOVE_DESTINATION
 } from "../actions/destination_actions";
 
-const destinationsReducer = (state = {}, action) => {
+const destinationsReducer = (state = [], action) => {
   Object.freeze(state);
+  let newState;
   switch (action.type) {
+    case RECEIVE_DESTINATIONS:
+        return state.action
+    case RECEIVE_DESTINATION:
+        if (state.length === 0) {
+                return [action.destination]
+            } else {
+                newState = Object.assign([], state);
+                let idx = null;
+                newState.forEach((destination, i) =>{
+                    if (destination._id === action.destination._id) {
+                        idx = i
+                    } 
+                })
+                if (idx) {
+                    newState[idx] = action.destination;
+                } else {
+                    newState.push(action.destination)
+                }
+                return newState
+            }
+    case REMOVE_DESTINATION:
+        newState = state.filter(destination => destination._id !== action.destinationId);
+        return newState;
     default:
       return state;
   }
