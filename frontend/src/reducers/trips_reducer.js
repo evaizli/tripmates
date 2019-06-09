c277cb7b21a002648fbcd5dbfc6bee560ae47f35
Jeff Dam <jeff.dam@gmail.com>
2019-06-08 20:36:33 -0700

    Correct trip reducer

--- frontend/src/reducers/trips_reducer.js
+++ frontend/src/reducers/trips_reducer.js
@@ -11,10 +11,25 @@ const tripsReducer = (state = [], action) => {
         case RECEIVE_TRIPS:
             return action.trips;
         case RECEIVE_TRIP:
-            newState = Object.assign([], state, [action.trip]);
-            return newState;
+            if (state.length === 0) {
+                return [action.trip];
+            } else {
+                newState = Object.assign([], state);
+                let idx;
+                newState.forEach((trip, i) =>{
+                    if (trip._id === action.trip._id) {
+                        idx = i;
+                    } 
+                });
+                if (idx !== undefined) {
+                    newState[idx] = action.trip;
+                } else {
+                    newState.push(action.trip);
+                }
+                return newState;
+            }
         case REMOVE_TRIP:
-            newState = state.filter(trip => trip._id !== action.tripId);
+            newState = state.filter((trip) => trip._id !== action.tripId);
             return newState;
         default:
             return state;
