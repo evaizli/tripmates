f330a0b93b22741ceb929c51be0aad0a590670a8
Jeff Dam <jeff.dam@gmail.com>
2019-06-08 20:25:04 -0700

    Fix trip reducer to prevent duplicate trips added to store

--- frontend/src/reducers/trips_reducer.js
+++ frontend/src/reducers/trips_reducer.js
@@ -4,43 +4,22 @@ import {
     REMOVE_TRIP 
 } from "../actions/trip_actions";
 
-
-
-
 const tripsReducer = (state = [], action ) => {
     Object.freeze(state);
     let newState;
     switch(action.type){
         case RECEIVE_TRIPS:
-            return action.trips
+            return action.trips;
         case RECEIVE_TRIP:
-            if (state.length === 0) {
-                return [action.trip]
-            } else {
-                newState = Object.assign([], state);
-                let idx = null;
-                newState.forEach((trip, i) =>{
-                    if (trip._id === action.trip._id) {
-                        idx = i
-                    } 
-                })
-                if (idx) {
-                    newState[idx] = action.trip;
-                } else {
-                    newState.push(action.trip)
-                }
-                return newState
-            }
+            newState = Object.assign([], state, [action.trip]);
+            return newState;
         case REMOVE_TRIP:
             newState = state.filter((trip) => trip._id !== action.tripId);
             return newState;
         default:
             return state;
-
     }
-
-}
-
+};
 
 export default tripsReducer;
  
