08334c35f62c08e362e89ef8b2e468861bddc4f0
Jeff Dam <jeff.dam@gmail.com>
2019-06-21 12:35:56 -0700

    Style splash buttons

--- frontend/src/components/splash/header/header.jsx
+++ frontend/src/components/splash/header/header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SessionButtonContainer from './header_session_buttons/session_button_container';
 
 const Header = () => {
@@ -10,9 +9,6 @@ const Header = () => {
         <h1>TripMates</h1>
         <SessionButtonContainer />
       </div>
-        <Link to={'/about-us'}>
-          <div className="about-us-btn">About Us</div>
-        </Link>
     </section>
   )
 }
